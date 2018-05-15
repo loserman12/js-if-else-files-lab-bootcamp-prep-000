251307faa26b88d1c086260be8258aefd1b15cc9
ralphcuello1016@gmail.com <ralphcuello1016@gmail.com>
2018-05-15 16:56:21 +0000

    automatically backed up by learn

---
+++ tests.js
@@ -0,0 +1,8 @@
+if (name === "Susan") {
+    console.log("The name is correct")
+} else {
+    console.log("Expected the name to be Susan")
+}
+if (height === 74){
+  console.log("The height is correct")
+}
